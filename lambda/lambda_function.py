612fd2d98e5eac3f91422d365a5092db4035d461
Ian Smith <ian@craftscale.uk>
2020-05-02 09:48:04 +0100

    Relaxed postcode regex
    Allow for leading & trailing spaces, and also put  \s in the middle instead of an actual space.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -21,7 +21,7 @@ else:
 
 if noisy:
     print("Loading lambda_function.py module")
-    
+
 # Exception for when permission has not been granted to retrieve the device postcode
 # from Amazon
 class PostcodeNoAuthorisation(Exception):
@@ -30,7 +30,7 @@ class PostcodeNoAuthorisation(Exception):
 # Exception for when countryCode is outside of the UK
 class OutOfGeographicalScope(Exception):
     pass
-    
+
 # Exception for when postcode doesn't match regex.
 class InvalidPostcode(Exception):
     pass
@@ -45,7 +45,7 @@ ask = Ask(app, "/")
 # Postcode regex matcher.
 def check_postcode(postcode):
     # Regex from https://stackoverflow.com/questions/164979/regex-for-matching-uk-postcodes
-    pcregex = re.compile(r'^([A-Z][A-HJ-Y]?[0-9][A-Z0-9]? ?[0-9][A-Z]{2}|GIR ?0A{2})$')
+    pcregex = re.compile(r'^\s*([A-Z][A-HJ-Y]?[0-9][A-Z0-9]?\s?[0-9][A-Z]{2}|GIR ?0A{2})\s*$')
 
     if pcregex.match(postcode.upper()):
         if noisy:
@@ -53,7 +53,7 @@ def check_postcode(postcode):
     else:
         print("Error: not a matching UK postcode: {}".format(postcode))
         return False
-            
+
     return True
 
 # Retrieve the cheapest slot
@@ -64,40 +64,40 @@ def get_timeframe(o, numberOfSlots):
     slotStart, slotFinish = o.getCheapestSlot(numberOfSlots * 30) # it takes minutes as arg
 
     # The times from the Octopus API are in UTC, so need converting if we're in summer
-    # time at the moment.       
+    # time at the moment.
     slotStart = slotStart.astimezone(uktz)
     slotFinish = slotFinish.astimezone(uktz)
-    
+
     return slotStart, slotFinish
 
 def get_postcode(deviceId, apiEndpoint, apiAccessToken):
-    
+
     requestURL = "{}/v1/devices/{}/settings/address/countryAndPostalCode".format(apiEndpoint, deviceId)
 
     requestHeader = {
         'Accept': 'application/json',
         'Authorization': 'Bearer {}'.format(apiAccessToken)
     }
-             
+
     r = requests.get(requestURL, headers=requestHeader)
-        
+
     if r.status_code == 403:
         raise PostcodeNoAuthorisation
-     
+
     if r.status_code == 200:
-        if r.json()['countryCode'] == 'GB':   
+        if r.json()['countryCode'] == 'GB':
             postcode = r.json()['postalCode']
         else:
             # The skill is only published to UK, but this added in response to Amazon review
             raise OutOfGeographicalScope
-    
+
     if r.status_code in [204, 404, 405, 429, 500]:
         print("Error: Unsupported error calling postcode retrieval API, status: {}".format(r.status_code))
         raise APIError
-    
+
     if check_postcode(postcode) != True: # this tests against a postcode matching regex...
         raise InvalidPostcode
-    
+
     return postcode
 
 
@@ -113,21 +113,21 @@ def slotLengthWords(numberOfSlots):
         result = str(numberOfSlots // 2)
         if numberOfSlots % 2 == 1:
             result = result + ' and a half'
-            
+
         result = result + ' hour'
-        
+
     return result
 
-    
+
 @ask.launch
 def start_skill():
     if noisy:
         print('Debug: start_skill() entered')
-        
+
     welcome_message = 'Hello there, I can tell you the cheapest time to do something that \
         uses a lot of electricity on your Agile Octopus tariff. How long a time slot do \
         you need?'
-        
+
     return question(welcome_message)
 
 
@@ -160,7 +160,7 @@ def find_cheapest_slot(Length):
         print("Error: Other Uncaught Error, so no postcode available")
         return statement("I'm so sorry, but I can't help - for some reason I can't \
             retrieve your device's postcode.")
-    
+
     # Recover gracefully if we didn't catch the slot length - probably won't happen
     # as we have elicitation on for the Length slot.
     if 'Length' in convert_errors:
@@ -181,7 +181,7 @@ def find_cheapest_slot(Length):
             Feel free to try again in a moment?")
 
     durationInSlots = Length.total_seconds() // 1800
-    
+
     # Less than half an hour is rounded up to half an hour.
     if durationInSlots == 0:
         durationInSlots = 1
@@ -197,14 +197,14 @@ def find_cheapest_slot(Length):
         return statement("I'm sorry, but my connection to Octopus Energy appears \
             to have gone a bit pear shaped, so I can't help you at the moment. \
             Feel free to try again in a moment?")
-    
+
     # otherwise...
     result = 'The cheapest {} slot runs from {} to {}'.format(
-            slotLengthWords(durationInSlots),
-            slotStart.strftime('%I:%M%p'), slotFinish.strftime('%I:%M%p'))
+        slotLengthWords(durationInSlots),
+        slotStart.strftime('%I:%M%p'), slotFinish.strftime('%I:%M%p'))
 
     print("Tariff: {}, Returned: {}".format(o.octopusGetTariffCode(), result))
-    
+
     return statement(result)
 
 # Manage Amazon's default intents...
